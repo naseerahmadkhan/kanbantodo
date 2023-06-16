e59dd6f3cdab3acfb8d87b18c6b2f9907fa7c4de
“naseer <naseer@edatasolutions.com.au>
2023-06-16 17:05:30 +1000

    login password schema refactored

--- src/screens/login/loginSchema.js
+++ src/screens/login/loginSchema.js
@@ -1,30 +1,32 @@
-import * as Yup from "yup";
-import validator from "validator";
+import * as Yup from 'yup';
+import validator from 'validator';
 
 export const LoginSchema = Yup.object().shape({
-   
+  email: Yup.string()
+    .email('Invalid email format')
+    .required('Valid email is required')
+    .test(
+      'is-valid',
+      message => `${message.path} is invalid`,
+      value =>
+        value
+          ? validator.isEmail(value)
+          : new Yup.ValidationError('Invalid value'),
+    ),
 
-    email: Yup.string()
-      .email("Invalid email format")
-      .required("Valid email is required")
-      .test(
-        "is-valid",
-        (message) => `${message.path} is invalid`,
-        (value) =>
-          value
-            ? validator.isEmail(value)
-            : new Yup.ValidationError("Invalid value")
-      ),
-
-    password: Yup.string()
-      // .min(8, 'Too Short!')
-      .max(16, "Too Long!")
-      .required("Valid password required")
-      .matches(
-        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
-        "Use 8 or more characters with a mix of letters, numbers & symbols"
-      ),
-    // https://regexr.com/3bfsi
-
-  
-  });
+  password: Yup.string()
+    // .min(8, 'Too Short!')
+    // .max(16, 'Too Long!')
+    // .required('Valid password required')
+    // .matches(
+    //   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+    //   'Use 8 or more characters with a mix of letters, numbers & symbols',
+    // ),
+    .required('Valid password required')
+    .min(8, 'Password must be 8 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    // .matches(/[A-Z]/, 'Password requires an uppercase letter')
+    // .matches(/[^\w]/, 'Password requires a symbol'),
+  // https://regexr.com/3bfsi
+});
